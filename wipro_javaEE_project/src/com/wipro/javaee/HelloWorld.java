71d7a1249c5b3069765daaf31bcb2e56bd3a1bff
Pratik <shubhsingh1311@gmail.com>
2025-02-01 10:17:20 +0530

    commit

---
+++ wipro_javaEE_project/src/com/wipro/javaee/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.javaee;
+
+/**
+ * @author Pratik1311
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
